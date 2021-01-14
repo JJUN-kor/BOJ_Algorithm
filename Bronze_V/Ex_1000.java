272ee5cf677eaacdc4275532993746aaf1ca109a
JJUN-kor <hj0687@knu.ac.kr>
2021-01-14 18:13:37 +0900

    1000번

---
+++ Bronze_V/Ex_1000.java
@@ -0,0 +1,22 @@
+package Bronze_V;
+
+//https://www.acmicpc.net/problem/1001
+
+import java.util.*;
+public class Ex_1000 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A,B,result; 
+		
+		A = sc.nextInt();
+		B = sc.nextInt();
+		result = A+B;
+		
+		System.out.println(result);
+		
+		sc.close();
+		
+		
+	}
+}
